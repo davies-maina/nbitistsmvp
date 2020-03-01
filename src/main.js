a8c22189dcb71d046b0cac8bda1e1ecce323fd8e
Davies Maina <daviesmaina@outlook.com>
2020-03-01 18:03:24 +0300

    bootstrap jquery popper instll

--- src/main.js
+++ src/main.js
@@ -1,10 +1,15 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
+import jQuery from "jquery";
+import "bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+global.jQuery = jQuery;
+global.$ = jQuery;
 
 Vue.config.productionTip = false;
 
 new Vue({
-  router,
-  render: h => h(App)
+    router,
+    render: h => h(App)
 }).$mount("#app");
